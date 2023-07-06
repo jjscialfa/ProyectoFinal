1e7168b451ae024f7c44a9ddaef95e43fbbe2c60
jjscialfa <jjscialfa@gmail.com>
2023-07-06 01:48:25 -0300

    segundo Commit

--- templates/js/productos.js
+++ templates/js/productos.js
@@ -6,7 +6,7 @@ createApp({
       productos: [],
       //url:'http://localhost:5000/productos',
       // si el backend esta corriendo local usar localhost 5000(si no lo subieron a pythonanywhere)
-      url: 'http://jjscialfa.pythonanywhere.com/productos', // si ya lo subieron a pythonanywhere
+      url: 'https://jjscialfa.pythonanywhere.com/productos', // si ya lo subieron a pythonanywhere
       error: false,
       cargando: true,
       /*atributos para el guardar los valores del formulario */
